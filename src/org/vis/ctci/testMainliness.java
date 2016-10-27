730511bcfc564cae969f306f2ad1846129717d20
selotape <RonVisbord@Gmail.com>
2016-10-26 17:15:59 -0700

    initial commit with dummy main

---
+++ src/org/vis/ctci/testMainliness.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vis.ctci;
+
+/**
+ * @author ronvis
+ *
+ */
+public class testMainliness {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java version is " + System.getProperty("java.version"));
+	}
+
+}
